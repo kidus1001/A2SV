88384ee1da528f2593352b8a740276de930162ac
Kidus1001 <154759967+kidus1001@users.noreply.github.com>
2025-06-03 13:46:46 +0300

    Created 'Palindrome Number' by A2SV Hub

---
+++ 03-Jun-2025/Palindrome Number 363088.py
@@ -0,0 +1,36 @@
+# Problem: Palindrome Number - https://leetcode.com/problems/palindrome-number/
+
+class Solution(object):
+    def isPalindrome(self, x):
+        #Optimal Answer
+        x = str(x)
+        pal = x[::-1]
+
+        if (x == pal):
+            return True
+        return False
+
+
+
+        # if (x<0):
+        #     return False
+        # numList = []
+        # while (x > 0):
+        #     singleDigitNum = x % 10
+        #     x /= 10
+        #     numList.append(singleDigitNum)
+        # left = 0
+        # right = len(numList)-1
+        # while (left < right):
+        #     if(numList[left] != numList[right]):
+        #         return False
+        #     left+=1
+        #     right-=1
+        
+        # return True
+        
+        """
+        :type x: int
+        :rtype: bool
+        """
+        
