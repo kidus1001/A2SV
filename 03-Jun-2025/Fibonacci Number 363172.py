7acbc4f10923ad6753d705c207cf7a702b9af18c
Kidus1001 <154759967+kidus1001@users.noreply.github.com>
2025-06-03 19:54:33 +0300

    Created 'Fibonacci Number' by A2SV Hub

---
+++ 03-Jun-2025/Fibonacci Number 363172.py
@@ -0,0 +1,14 @@
+# Problem: Fibonacci Number - https://leetcode.com/problems/fibonacci-number/
+
+class Solution(object):
+    def fib(self, n):
+        if (n == 0):
+            return 0
+        if (n == 1):
+            return 1
+        return (self.fib (n-1) + self.fib (n-2))
+        """
+        :type n: int
+        :rtype: int
+        """
+        
