1106275921231965854c4d95347314f6b3b2eda2
Kidus1001 <154759967+kidus1001@users.noreply.github.com>
2025-06-05 19:13:25 +0300

    Created 'Watermelon' by A2SV Hub

---
+++ 05-Jun-2025/Watermelon 363935.py
@@ -0,0 +1,11 @@
+# Problem: Watermelon - https://codeforces.com/problemset/problem/4/A
+
+n = int (input())
+ 
+ 
+if (n <= 2):
+     print ("NO")
+elif n%2 == 0:
+     print ("YES")
+else:
+     print ("NO")
