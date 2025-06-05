213a15409725ee227fa01d8f42176fd3b53be580
Kidus1001 <154759967+kidus1001@users.noreply.github.com>
2025-06-05 19:07:47 +0300

    Created 'Way Too Long Words' by A2SV Hub

---
+++ 05-Jun-2025/Way Too Long Words 363933.py
@@ -0,0 +1,14 @@
+# Problem: Way Too Long Words - https://codeforces.com/problemset/problem/71/A
+
+n =int(input())
+
+for words in range (n):
+     word = input()
+     if (len(word) > 10):
+          size = len(word)-2
+          firstCharacter = word[0]
+          lastCharacter = word[len(word)-1]
+          print(firstCharacter+str(size)+lastCharacter)
+     else:
+          print (word)
+          
