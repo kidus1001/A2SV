ef4efc08ec78e3e9dcce93ac544b7c118ad803cd
Kidus1001 <154759967+kidus1001@users.noreply.github.com>
2025-06-13 21:33:49 +0300

    Created 'Team' by A2SV Hub

---
+++ 13-Jun-2025/Team 366468.py
@@ -0,0 +1,14 @@
+# Problem: Team - https://codeforces.com/contest/231/problem/A
+
+n =int(input())
+
+total = 0
+for problem in range(n):
+     numLine = list(map(int, input().split(' ')))
+     count=0
+     for x in numLine:
+          if x == 1:
+               count+=1
+     if (count >= 2):
+          total+=1
+print (total)
