8034a6cdb38cd945360f648982041b0006e5f7c7
Kidus1001 <154759967+kidus1001@users.noreply.github.com>
2025-06-14 22:24:23 +0300

    Created 'A. Beautiful Matrix' by A2SV Hub

---
+++ 14-Jun-2025/A. Beautiful Matrix 366842.py
@@ -0,0 +1,17 @@
+# Problem: A. Beautiful Matrix - https://codeforces.com/problemset/problem/263/A
+
+x = 0
+y = 0
+
+matrix = []
+for i in range(5):
+     line = list(map(int, input().split()))
+     matrix.append(line)
+     
+for i in range(5):
+     for j in range(5):
+          if (matrix[i][j] == 1):
+               x = i
+               y = j
+result = abs(x-2) + abs(y-2)
+print (result)
