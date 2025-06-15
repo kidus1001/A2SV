fdc56b1d1365b1a9edbe659c28f6c386aa58b716
Kidus1001 <154759967+kidus1001@users.noreply.github.com>
2025-06-15 22:15:23 +0300

    Created 'A - Letter Duel' by A2SV Hub

---
+++ A2SV Remote contest #17 15-Jun-2025/A - Letter Duel 367023.py
@@ -0,0 +1,19 @@
+# Problem: A - Letter Duel - https://codeforces.com/gym/614464/problem/A
+
+n = int(input())
+
+for problems in range(n):
+     flag = True
+     l = 0
+     r = 0
+     stringSize = int(input())
+     myStringInput = input()
+     for i in range(stringSize-1):
+          if (myStringInput[i]!=myStringInput[i+1]):
+               flag = False
+               l = i+1
+               r = i+2
+     if (flag == False):
+          print (l, r)
+     else:
+          print (-1,-1)
