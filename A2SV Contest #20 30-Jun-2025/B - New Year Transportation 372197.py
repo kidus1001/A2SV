fd6c0534c5cc0f11110a551959e470020b2ad410
Kidus1001 <154759967+kidus1001@users.noreply.github.com>
2025-06-30 22:37:02 +0300

    Created 'B - New Year Transportation' by A2SV Hub

---
+++ A2SV Contest #20 30-Jun-2025/B - New Year Transportation 372197.py
@@ -0,0 +1,19 @@
+# Problem: B - New Year Transportation - https://codeforces.com/gym/618729/problem/B
+
+n, destination = map(int, input().split())
+ 
+line = list(map(int, input().split()))
+ 
+line.insert(0, 0)
+ 
+i = 1
+ 
+while (i <= destination):
+     if (i == destination):
+          print ("YES")
+          break
+     i += line[i]
+     
+     if (i > destination):
+          print("NO")
+          break
