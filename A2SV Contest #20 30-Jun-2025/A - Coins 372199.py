3a21b5dcccffe9b82076eeb3a4457d0547cf9b8f
Kidus1001 <154759967+kidus1001@users.noreply.github.com>
2025-06-30 22:37:19 +0300

    Created 'A - Coins' by A2SV Hub

---
+++ A2SV Contest #20 30-Jun-2025/A - Coins 372199.py
@@ -0,0 +1,11 @@
+# Problem: A - Coins - https://codeforces.com/gym/618729/problem/A
+
+n = int(input())
+
+for _ in range(n):
+     a, b = map(int, input().split())
+     if b%2 == 0 and a % 2 != 0:
+          print ("NO")
+     else:
+          print("YES")
+          
