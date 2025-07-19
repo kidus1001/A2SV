37937ab11952b68efa2879f710c491f7f55a9b15
Kidus1001 <154759967+kidus1001@users.noreply.github.com>
2025-07-19 23:04:20 +0300

    Created 'A - Swap to Max' by A2SV Hub

---
+++ G6 A2SV Remote Contest #22 19-Jul-2025/A - Swap to Max 377987.py
@@ -0,0 +1,15 @@
+# Problem: A - Swap to Max - https://codeforces.com/gym/622136/problem/A
+
+testCases = int(input())
+for _ in range(testCases):
+     n = int(input())
+     arr1 = list(map(int, input().split()))
+     arr2 = list(map(int, input().split()))
+     for i in range (n):
+          if (arr1[i]>arr2[i]):
+               arr1[i], arr2[i] = arr2[i], arr1[i]
+     
+     num1 = max(arr1)
+     num2 = max(arr2)
+     
+     print(num1*num2)
