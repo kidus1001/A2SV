307ccefb6e9920fda7bdcfe6476be31c027f7ac5
Kidus1001 <154759967+kidus1001@users.noreply.github.com>
2025-07-24 07:14:18 +0300

    Created 'C - The Double-Holed Culprit' by A2SV Hub

---
+++ G6 A2SV Remote Contest #22 24-Jul-2025/C - The Double-Holed Culprit 378959.py
@@ -0,0 +1,12 @@
+# Problem: C - The Double-Holed Culprit - https://codeforces.com/gym/622136/problem/C
+
+n = int(input())
+p = list(map(int, input().split()))
+
+for a in range(n):
+    visited = set()
+    current = a
+    while current not in visited:
+        visited.add(current)
+        current = p[current] - 1
+    print(current + 1, end=' ')
