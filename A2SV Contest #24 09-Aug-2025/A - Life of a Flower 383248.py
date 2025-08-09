061c99488c16d3e01241d62fb6c29e3d476782ba
Kidus1001 <154759967+kidus1001@users.noreply.github.com>
2025-08-10 00:31:24 +0300

    Created 'A - Life of a Flower' by A2SV Hub

---
+++ A2SV Contest #24 09-Aug-2025/A - Life of a Flower 383248.py
@@ -0,0 +1,23 @@
+# Problem: A - Life of a Flower - https://codeforces.com/gym/626626/problem/A
+
+testCases = int(input())
+for _ in range(testCases):
+     initialHeight = 1
+     size = int(input())
+     line = list(map(int, input().split()))
+     
+     for i in range (size):
+
+          if line[i] == 1:
+               initialHeight += 1
+               if i>0 and line[i-1]==1:
+                    initialHeight += 4
+          elif i>0 and line[i] == 0 and line[i-1] == 0:
+               initialHeight =-1
+               break
+               
+               
+          
+          
+          
+     print(initialHeight)
