e3e45e70b1541834cfd1e1cc1730c21f42618d3a
Kidus1001 <154759967+kidus1001@users.noreply.github.com>
2025-08-15 22:03:24 +0300

    Created 'A - Melody Perfection' by A2SV Hub

---
+++ A2SV G6 Remote Contest #25 15-Aug-2025/A - Melody Perfection 385603.py
@@ -0,0 +1,18 @@
+# Problem: A - Melody Perfection - https://codeforces.com/gym/628023/problem/A
+
+testCases = int(input())
+
+for _ in range (testCases):
+     perfectCheck = True
+     size = int(input())
+     melodies = list(map(int, input().split()))
+     
+     for i in range(len(melodies)-1):
+          res = abs(melodies[i] - melodies[i+1])
+          if not(res == 5 or res == 7):
+               perfectCheck = False
+               break
+     if perfectCheck:
+          print ("YES")
+     else:
+          print("NO")
