cf0c612d557c2a19c3881b0e0d0e40ad82a2f85c
Kidus1001 <154759967+kidus1001@users.noreply.github.com>
2025-08-15 22:01:19 +0300

    Created 'B - Kinda Swap, Kinda Rock' by A2SV Hub

---
+++ A2SV G6 Remote Contest #25 15-Aug-2025/B - Kinda Swap, Kinda Rock 385600.py
@@ -0,0 +1,19 @@
+# Problem: B - Kinda Swap, Kinda Rock - https://codeforces.com/gym/628023/problem/B
+
+testCases = int(input())
+for _ in range(testCases):
+     n, swap = list(map(int, input().split()))
+     arr1 = list(map(int, input().split()))
+     arr2 = list(map(int, input().split()))
+     
+     arr1.sort()
+     arr2.sort(reverse=True)
+     
+     for i in range(swap):
+          if (arr2[i]>arr1[i]):
+               arr1[i], arr2[i] = arr2[i], arr1[i]
+          else:
+               break
+     
+     print(sum(arr1))
+     
