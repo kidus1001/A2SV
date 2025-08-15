1a5d39b2ec0aef9e3cf2df72deee0564019da7e9
Kidus1001 <154759967+kidus1001@users.noreply.github.com>
2025-08-15 22:01:10 +0300

    Created 'E - Zeroing Array' by A2SV Hub

---
+++ A2SV G6 Remote Contest #25 15-Aug-2025/E - Zeroing Array 385598.py
@@ -0,0 +1,16 @@
+# Problem: E - Zeroing Array - https://codeforces.com/gym/628023/problem/E
+
+from sys import stdin
+
+n = int(stdin.readline().strip())
+arr = list(map(int, stdin.readline().strip().split()))
+
+total = max_value = 0
+for a in arr:
+    total += a
+    max_value = max(max_value, a)
+    
+if total % 2 == 1  or  max_value > total - max_value:
+    print("NO")
+else:
+    print("YES")
