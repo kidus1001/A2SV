9cfe81bf886411de823b6347d0a863ca19471406
Kidus1001 <154759967+kidus1001@users.noreply.github.com>
2025-08-18 22:55:14 +0300

    Created 'B - Maxim vs. Letters: Newspaper Heist' by A2SV Hub

---
+++ A2SV Contest #26 18-Aug-2025/B - Maxim vs. Letters: Newspaper Heist 386675.py
@@ -0,0 +1,16 @@
+# Problem: B - Maxim vs. Letters: Newspaper Heist - https://codeforces.com/gym/629689/problem/B
+
+from collections import Counter
+
+s1 = input().rstrip('\n')
+s2 = input().rstrip('\n')
+
+cnt1 = Counter(ch for ch in s1 if ch != ' ')
+cnt2 = Counter(ch for ch in s2 if ch != ' ')
+
+for ch in cnt2:
+    if cnt2[ch] > cnt1[ch]:
+        print("NO")
+        break
+else:
+    print("YES")
