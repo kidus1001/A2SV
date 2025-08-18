e324cf252b7d0390b0357a04cd882c244d05904c
Kidus1001 <154759967+kidus1001@users.noreply.github.com>
2025-08-18 17:38:17 +0300

    Created 'A - Maxim vs. Yogurt: The Great Discount Battle' by A2SV Hub

---
+++ A2SV Contest #26 18-Aug-2025/A - Maxim vs. Yogurt: The Great Discount Battle 386500.py
@@ -0,0 +1,12 @@
+# Problem: A - Maxim vs. Yogurt: The Great Discount Battle - https://codeforces.com/gym/629689/problem/A
+
+testCases = int(input())
+for _ in range(testCases):
+     n, priceOne, promotional = map(int, input().split())
+     if ((promotional/2.0) > priceOne):
+          print (n*priceOne)
+     else:
+          if (n%2 == 0):
+               print (n//2 * promotional)
+          else:
+               print ((n//2 * promotional) + priceOne)
