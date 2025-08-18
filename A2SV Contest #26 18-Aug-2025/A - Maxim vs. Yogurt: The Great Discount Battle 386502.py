e361c8ddd900efa5ca600cb307bb9221351e2cdc
Kidus1001 <154759967+kidus1001@users.noreply.github.com>
2025-08-18 17:38:34 +0300

    Created 'A - Maxim vs. Yogurt: The Great Discount Battle' by A2SV Hub

---
+++ A2SV Contest #26 18-Aug-2025/A - Maxim vs. Yogurt: The Great Discount Battle 386502.py
@@ -0,0 +1,9 @@
+# Problem: A - Maxim vs. Yogurt: The Great Discount Battle - https://codeforces.com/gym/629689/problem/A
+
+testCases = int(input())
+for _ in range(testCases):
+     n, priceOne, promotional = map(int, input().split())
+     
+     costNormal = n*priceOne
+     costPromo = ((n//2) * promotional) + (n%2) * priceOne
+     print (min(costNormal, costPromo))
