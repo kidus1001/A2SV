bc45686ee4c44a3495a640485479b84737a20323
Kidus1001 <154759967+kidus1001@users.noreply.github.com>
2025-08-21 00:02:32 +0300

    Created 'Number Complement' by A2SV Hub

---
+++ 20-Aug-2025/Number Complement 387327.py
@@ -0,0 +1,14 @@
+# Problem: Number Complement - https://leetcode.com/problems/number-complement/
+
+class Solution(object):
+    def findComplement(self, num):
+        bit_length = num.bit_length()
+        
+        mask = (1 << bit_length) - 1
+        
+        return num ^ mask
+        """
+        :type num: int
+        :rtype: int
+        """
+        
