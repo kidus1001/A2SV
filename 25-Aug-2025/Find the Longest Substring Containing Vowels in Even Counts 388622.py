71d201c5361cc09396e461640970f52324b0216f
Kidus1001 <154759967+kidus1001@users.noreply.github.com>
2025-08-25 04:33:56 +0300

    Created 'Find the Longest Substring Containing Vowels in Even Counts' by A2SV Hub

---
+++ 25-Aug-2025/Find the Longest Substring Containing Vowels in Even Counts 388622.py
@@ -0,0 +1,17 @@
+# Problem: Find the Longest Substring Containing Vowels in Even Counts - https://leetcode.com/problems/find-the-longest-substring-containing-vowels-in-even-counts/
+
+class Solution(object):
+    def findTheLongestSubstring(self, s):
+        vowels = {'a':0, 'e':1, 'i':2, 'o':3, 'u':4}
+        mask = 0
+        seen = {0:-1}
+        ans = 0
+
+        for i, char in enumerate (s):
+            if char in vowels:
+                mask ^= (1 << vowels[char])
+            if mask in seen:
+                ans = max(ans, i-seen[mask])
+            else:
+                seen[mask] = i
+        return ans
