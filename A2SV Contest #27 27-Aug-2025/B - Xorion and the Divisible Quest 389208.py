a99c78e9a4656fe3cf67e5b2d0d5e6e06c49ad8a
Kidus1001 <154759967+kidus1001@users.noreply.github.com>
2025-08-27 04:26:11 +0300

    Created 'B - Xorion and the Divisible Quest' by A2SV Hub

---
+++ A2SV Contest #27 27-Aug-2025/B - Xorion and the Divisible Quest 389208.py
@@ -0,0 +1,14 @@
+# Problem: B - Xorion and the Divisible Quest - https://codeforces.com/gym/630556/problem/B
+
+n = int(input())
+listGiven = list(map(int, input().split()))
+answer = min(listGiven)
+check = True
+for i in listGiven:
+     if i%answer!=0:
+          check=False
+          break
+if (check):
+     print (answer)
+else:
+     print(-1)
