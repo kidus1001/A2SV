29a4df982bb9cbcbbd1b89be658f4f355b3bbd86
Kidus1001 <154759967+kidus1001@users.noreply.github.com>
2025-08-28 17:26:18 +0300

    Created 'Merge k Sorted Lists' by A2SV Hub

---
+++ 28-Aug-2025/Merge k Sorted Lists 389525.py
@@ -0,0 +1,36 @@
+# Problem: Merge k Sorted Lists - https://leetcode.com/problems/merge-k-sorted-lists/
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeKLists(self, lists):
+        if not lists:
+            return None
+        if len(lists) == 1:
+            return lists[0]
+        
+        mid = len(lists) // 2
+        left = self.mergeKLists(lists[:mid])
+        right = self.mergeKLists(lists[mid:])
+        
+        return self.merge(left, right)
+    
+    def merge(self, l1, l2):
+        dummy = ListNode(0)
+        curr = dummy
+        
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+        
+        curr.next = l1 or l2
+        
+        return dummy.next
