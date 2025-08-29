cb5bd57aba4bb50abba1cdbb7fa61d5ab844672e
Kidus1001 <154759967+kidus1001@users.noreply.github.com>
2025-08-29 21:33:57 +0300

    Created 'Sum of Two Integers' by A2SV Hub

---
+++ 29-Aug-2025/Sum of Two Integers 389944.py
@@ -0,0 +1,11 @@
+# Problem: Sum of Two Integers - https://leetcode.com/problems/sum-of-two-integers/description/
+
+class Solution(object):
+    def getSum(self, a, b):
+        return a+b
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        
