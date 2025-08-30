38e2649e7777ff11a3516d1b44e25ec59ce9b2e5
Kidus1001 <154759967+kidus1001@users.noreply.github.com>
2025-08-31 00:03:59 +0300

    Created 'A - Card Game' by A2SV Hub

---
+++ A2SV Remote Education Contest #18 30-Aug-2025/A - Card Game 390245.py
@@ -0,0 +1,14 @@
+# Problem: A - Card Game - https://codeforces.com/gym/616066/problem/A
+
+n = int(input())
+
+for _ in range(n):
+     line = list(map(int, input().split()))
+     
+     line1 = list(map(int, input().split()))
+     line2 = list(map(int, input().split()))
+     
+     if max(line1) > max(line2):
+          print("YES")
+     else:
+          print("NO")
