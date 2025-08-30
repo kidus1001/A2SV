eb5f78eaafff74bbee441b57fc0757087bdeb9ec
Kidus1001 <154759967+kidus1001@users.noreply.github.com>
2025-08-31 00:04:08 +0300

    Created 'B - 01 Game' by A2SV Hub

---
+++ A2SV Remote Education Contest #18 30-Aug-2025/B - 01 Game 390246.py
@@ -0,0 +1,24 @@
+# Problem: B - 01 Game - https://codeforces.com/gym/616066/problem/B
+
+n = int(input())
+
+for _ in range (n):
+     binaryInput = input()
+     zeroCount = 0
+     oneCount = 0
+     for ch in range(len(binaryInput)):
+          if binaryInput[ch] == '0':
+               zeroCount+=1
+          else:
+               oneCount+=1
+     pairs = 0
+     if (zeroCount > oneCount):
+          pairs = oneCount
+     else:
+          pairs = zeroCount
+     
+     if (pairs % 2 == 0):
+          print("NET")
+     else:
+          print("DA")
+          
