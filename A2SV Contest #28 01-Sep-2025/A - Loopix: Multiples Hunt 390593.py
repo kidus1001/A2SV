eda5e860f999692a55cd1513366688607a70f9c9
Kidus1001 <154759967+kidus1001@users.noreply.github.com>
2025-09-01 19:51:51 +0300

    Created 'A - Loopix: Multiples Hunt' by A2SV Hub

---
+++ A2SV Contest #28 01-Sep-2025/A - Loopix: Multiples Hunt 390593.py
@@ -0,0 +1,12 @@
+# Problem: A - Loopix: Multiples Hunt - https://codeforces.com/gym/632067/problem/A
+
+import math
+testCases = int(input())
+for _ in range(testCases):
+     l, r, k = map(int, input().split())
+     i = (r+1)/k
+     k = math.ceil(i)
+     if (k<l):
+          print (0)
+     else:
+          print(k-l)
