e28d2c9e8cf6ad97ed1dec1e5cd193db801cda44
Kidus1001 <154759967+kidus1001@users.noreply.github.com>
2025-09-13 23:33:20 +0300

    Created 'Almost Prime' by A2SV Hub

---
+++ 13-Sep-2025/Almost Prime 393039.py
@@ -0,0 +1,12 @@
+# Problem: Almost Prime - https://codeforces.com/problemset/problem/26/A
+
+n = int(input())
+cnt = [0] * (n + 1)
+
+for p in range(2, n + 1):
+    if cnt[p] == 0:  # p is prime
+        for k in range(p, n + 1, p):
+            cnt[k] += 1  # count distinct prime factor
+
+ans = sum(1 for i in range(1, n + 1) if cnt[i] == 2)
+print(ans)
