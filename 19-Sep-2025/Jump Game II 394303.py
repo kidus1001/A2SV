5ebf1f81f2bec65d5056d3c2987caebc6d65dd7b
Kidus1001 <154759967+kidus1001@users.noreply.github.com>
2025-09-19 23:34:25 +0300

    Created 'Jump Game II' by A2SV Hub

---
+++ 19-Sep-2025/Jump Game II 394303.py
@@ -0,0 +1,18 @@
+# Problem: Jump Game II - https://leetcode.com/problems/jump-game-ii/description/
+
+class Solution:
+  def jump(self, nums: List[int]) -> int:
+    ans = 0
+    end = 0
+    farthest = 0
+
+    for i in range(len(nums) - 1):
+      farthest = max(farthest, i + nums[i])
+      if farthest >= len(nums) - 1:
+        ans += 1
+        break
+      if i == end:      
+        ans += 1        
+        end = farthest  
+
+    return ans
