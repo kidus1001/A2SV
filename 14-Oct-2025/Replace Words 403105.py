1f20723306fc458557797a285bf147b589475644
Kidus1001 <154759967+kidus1001@users.noreply.github.com>
2025-10-14 21:34:37 +0300

    Created 'Replace Words' by A2SV Hub

---
+++ 14-Oct-2025/Replace Words 403105.py
@@ -0,0 +1,18 @@
+# Problem: Replace Words - https://leetcode.com/problems/replace-words/
+
+class Solution:
+    def replaceWords(self, dict: List[str], sentence: str) -> str:
+        roots = set(dict)
+        words = sentence.split()
+        result = []
+
+        for word in words:
+            for i in range(len(word) + 1):
+                prefix = word[:i]
+                if prefix in roots:
+                    result.append(prefix)
+                    break
+            else:
+                result.append(word)
+
+        return ' '.join(result)
