56165671f074560fa410c7878ca71dc806ab10ef
Kidus1001 <154759967+kidus1001@users.noreply.github.com>
2025-10-14 21:49:15 +0300

    Created 'Short Encoding of Words' by A2SV Hub

---
+++ 14-Oct-2025/Short Encoding of Words 403117.py
@@ -0,0 +1,12 @@
+# Problem: Short Encoding of Words - https://leetcode.com/problems/short-encoding-of-words/
+
+class Solution:
+    def minimumLengthEncoding(self, words):
+            root = dict()
+            leaves = []
+            for word in set(words):
+                cur = root
+                for i in word[::-1]:
+                    cur[i] = cur = cur.get(i, dict())
+                leaves.append((cur, len(word) + 1))
+            return sum(depth for node, depth in leaves if len(node) == 0)
